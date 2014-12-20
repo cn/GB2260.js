b7a2564200f4becc1f07da83b5677e97ece114f4
Hsiaoming Yang <me@lepture.com>
2014-12-20 15:09:51 +0800

    Update generate scripts

--- generate.js
+++ generate.js
@@ -3,14 +3,37 @@
  */
 
 var fs = require('fs');
-var text = fs.readFileSync(__dirname + '/../GB2260.txt', 'utf8');
 
-var lines = text.trim().split('\n');
+var CURRENT = 2013;
+var LISTS = [2012, 2006];
+
+function parse(text) {
+  var lines = text.trim().split('\n');
+  var rv = {};
+
+  lines.forEach(function(line) {
+    var bits = line.split('\t');
+    rv[bits[0]] = bits[1];
+  });
+
+  return rv;
+}
+
+function read(year) {
+  var name = 'GB2260.txt';
+  if (year) {
+    name = 'GB2260-' + year + '.txt';
+  }
+  return fs.readFileSync(__dirname + '/data/' + name, 'utf8');
+}
+
 var rv = {};
+rv[CURRENT] = parse(read());
 
-lines.forEach(function(line) {
-  var bits = line.split('\t');
-  rv[bits[0]] = bits[1];
-});
+var i = LISTS[0];
+while (i >= LISTS[1]) {
+  rv[i] = parse(read(i));
+  i = i - 1;
+}
 
 console.log(JSON.stringify(rv, null, 2));
