9bdb43e9b751bfe3bda8d0f6695cd5d61cf584eb
Hsiaoming Yang <me@lepture.com>
2015-09-25 14:58:12 +0800

    Add revisions data

--- scripts/generate.js
+++ scripts/generate.js
@@ -36,13 +36,28 @@ function parse(text) {
   return rv;
 }
 
-var names = fs.readdirSync('data');
-names.forEach(function(name) {
+var revisions = [];
+
+fs.readdirSync('data').forEach(function(name) {
   var m = name.match(/^GB2260-(\d+).txt$/);
   if (!m) return;
   var revision = m[1];
+  revisions.push(revision);
   var text = fs.readFileSync(__dirname + '/../data/' + name, 'utf8');
   var data = JSON.stringify(parse(text), null, 2);
   console.log('write ' + revision);
   fs.writeFileSync(__dirname + '/../lib/' + revision + '.json', data);
 });
+
+revisions.sort(function(a, b) {
+  var aYear = parseInt(a.substr(0, 4), 10);
+  var bYear = parseInt(b.substr(0, 4), 10);
+  if (aYear != bYear) {
+    return bYear - aYear;
+  }
+
+  var aMonth = parseInt(a.substr(4, 6) || 13, 10);
+  var bMonth = parseInt(b.substr(4, 6) || 13, 10);
+  return bMonth - aMonth;
+});
+fs.writeFileSync(__dirname + '/../lib/revisions.json', JSON.stringify(revisions));
