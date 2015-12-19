bbe69452c011aa26b0eefa88b1edcc935061bd31
Hsiaoming Yang <me@lepture.com>
2015-12-19 18:46:25 +0800

    Add contrib data

--- scripts/generate.js
+++ scripts/generate.js
@@ -18,28 +18,47 @@ function parse(text) {
   return rv;
 }
 
-var revisions = [];
 
-fs.readdirSync('data').forEach(function(name) {
-  var m = name.match(/^(\d+).tsv/);
-  if (!m) return;
-  var revision = m[1];
-  revisions.push(revision);
-  var text = fs.readFileSync(__dirname + '/../data/' + name, 'utf8');
-  var data = JSON.stringify(parse(text), null, 2);
-  console.log('write ' + revision);
-  fs.writeFileSync(__dirname + '/../lib/' + revision + '.json', data);
-});
+function createJSON() {
+  var revisions = [];
 
-revisions.sort(function(a, b) {
-  var aYear = parseInt(a.substr(0, 4), 10);
-  var bYear = parseInt(b.substr(0, 4), 10);
-  if (aYear != bYear) {
-    return bYear - aYear;
-  }
+  fs.readdirSync('data').forEach(function(name) {
+    var m = name.match(/^(\d+).tsv/);
+    if (!m) return;
+    var revision = m[1];
+    revisions.push(revision);
+    var text = fs.readFileSync(__dirname + '/../data/' + name, 'utf8');
+    var data = JSON.stringify(parse(text), null, 2);
+    console.log('write ' + revision);
+    fs.writeFileSync(__dirname + '/../lib/' + revision + '.json', data);
+  });
 
-  var aMonth = parseInt(a.substr(4, 6) || 13, 10);
-  var bMonth = parseInt(b.substr(4, 6) || 13, 10);
-  return bMonth - aMonth;
-});
-fs.writeFileSync(__dirname + '/../lib/revisions.json', JSON.stringify(revisions));
+  revisions.sort(function(a, b) {
+    var aYear = parseInt(a.substr(0, 4), 10);
+    var bYear = parseInt(b.substr(0, 4), 10);
+    if (aYear != bYear) {
+      return bYear - aYear;
+    }
+
+    var aMonth = parseInt(a.substr(4, 6) || 13, 10);
+    var bMonth = parseInt(b.substr(4, 6) || 13, 10);
+    return bMonth - aMonth;
+  });
+  fs.writeFileSync(__dirname + '/../lib/revisions.json', JSON.stringify(revisions));
+}
+
+
+function createContrib() {
+  fs.readdirSync('data/contrib').forEach(function(name) {
+    var m = name.match(/^(.+).tsv/);
+    if (!m) return;
+    var filename = m[1];
+    var text = fs.readFileSync(__dirname + '/../data/contrib/' + name, 'utf8');
+    var data = JSON.stringify(parse(text), null, 2);
+    console.log('write ' + filename);
+    fs.writeFileSync(__dirname + '/../lib/' + filename + '.json', data);
+  });
+}
+
+createJSON()
+createContrib()
